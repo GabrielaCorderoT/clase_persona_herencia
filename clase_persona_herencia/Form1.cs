f487b7f9780a915f5d0e4a2d7bc50c4c22ffe443
GabrielaCorderoT <163442806+GabrielaCorderoT@users.noreply.github.com>
2024-05-09 00:03:12 -0600

    Add files via upload

---
+++ clase_persona_herencia/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clase_persona_herencia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void limpiar_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+        }
+
+        private void Salir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void guardar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Datos guardados");
+        }
+    }
+}
---
+++ clase_persona_herencia/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clase_persona_herencia
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+    //el método se va a repetir en las 4 clases para pedir la edad, el nombre y la fecha de nacimiento
+    //clase papá 
+    //creamos una clase pública para persona
+    public class Persona
+    {
+        //creamos un string para nombre y fecha de nacimiento porque es un elemento de texto
+        //creamos un elemento int para edad porque es un elemento numérico
+        //utilizamos get y set para que tome los datos y los devuelva
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        public string Fechanacimiento { get; set; }
+        public Persona(string nombre, int edad, string fechanacimiento)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Fechanacimiento = fechanacimiento;
+        }
+
+        //este comando nos permite mostrar la información solicitada 
+        public void MostarInformacion()
+        {
+            Console.WriteLine($"Nombre: {Nombre} Edad: {Edad} Fechanacimiento: {Fechanacimiento}");
+        }
+    }
+
+    //clase hija 
+    //creamos una clase pública para alumno
+    public class Alumno
+    {
+        //creamos un string para nombre y fecha de nacimiento porque es un elemento de texto
+        //creamos un elemento int para edad porque es un elemento numérico
+        //utilizamos get y set para que tome los datos y los devuelva
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        public string Fechanacimiento { get; set; }
+        public Alumno(string nombre, int edad, string fechanacimiento)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Fechanacimiento = fechanacimiento;
+        }
+
+        //este comando nos permite mostrar la información solicitada
+        public void MostarInformacion()
+        {
+            Console.WriteLine($"Nombre: {Nombre} Edad: {Edad} Fechanacimiento: {Fechanacimiento}");
+        }
+    }
+
+    //clase hija
+    //creamos una clase pública para empleado
+    public class Empleado
+    {
+        //creamos un string para nombre y fecha de nacimiento porque es un elemento de texto
+        //creamos un elemento int para edad porque es un elemento numérico
+        //utilizamos get y set para que tome los datos y los devuelva
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        public string Fechanacimiento { get; set; }
+        public Empleado(string nombre, int edad, string fechanacimiento)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Fechanacimiento = fechanacimiento;
+        }
+
+        //este comando nos permite mostrar la información solicitada
+        public void MostarInformacion()
+        {
+            Console.WriteLine($"Nombre: {Nombre} Edad: {Edad} Fechanacimiento: {Fechanacimiento}");
+        }
+    }
+
+    //clase hija
+    //creamos una clase pública para docente
+    public class Docente
+    {
+        //creamos un string para nombre y fecha de nacimiento porque es un elemento de texto
+        //creamos un elemento int para edad porque es un elemento numérico
+        //utilizamos get y set para que tome los datos y los devuelva
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        public string Fechanacimiento { get; set; }
+        public Docente(string nombre, int edad, string fechanacimiento)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Fechanacimiento = fechanacimiento;
+        }
+
+        //este comando nos permite mostrar la información solicitada
+        public void MostarInformacion()
+        {
+            Console.WriteLine($"Nombre: {Nombre} Edad: {Edad} Fechanacimiento: {Fechanacimiento}");
+        }
+    }
+}
+
+
+
